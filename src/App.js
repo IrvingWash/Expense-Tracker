cfd5acf3edba8858f9d7cdb71735c3eff63e36fe
Washington Irving <iamwashingyou@gmail.com>
2021-06-18 01:39:51 +0300

    added hardcoded expense item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import ExpensetItem from './components/ExpenseItem';
+
 const App = () => {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h2>Hello</h2>
+
+      <ExpensetItem />
+    </div>
+  );
 };
 
 export default App;
