b23449446c7a4ea08ea7101a4edd03d3f560d74c
Washington Irving <iamwashingyou@gmail.com>
2021-06-18 01:58:49 +0300

    added dynamic content to ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import ExpensetItem from './components/ExpenseItem';
 
 const App = () => {
+  const expenses = [
+    {
+      title: 'Car Insurance',
+      amount: 15000,
+      date: new Date(2021, 5, 18),
+    },
+    {
+      title: 'Tea',
+      amount: 250,
+      date: new Date(2021, 4, 12),
+    },
+    {
+      title: 'Headphones',
+      amount: 5000,
+      date: new Date(2021, 1, 1),
+    },
+  ];
+
   return (
     <div className="App">
       <h2>Hello</h2>
 
-      <ExpensetItem />
+      <ExpensetItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpensetItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpensetItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
     </div>
   );
 };
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+  const { title, amount, date } = props;
+
   return (
     <div className="expense-item">
-      <div>Jun 18th 2021</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
       </div>
     </div>
   );
