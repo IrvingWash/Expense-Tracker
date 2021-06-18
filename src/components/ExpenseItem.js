bf87e31fc7860743b0a945348574d895924a5bc2
Washington Irving <iamwashingyou@gmail.com>
2021-06-19 02:35:26 +0300

    Destructurized and stylized the expense date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,16 @@
 import './ExpenseItem.css';
 
+import ExpenseDate from './ExpenseDate';
+
 const ExpenseItem = (props) => {
   const { title, amount, date } = props;
 
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">{amount}</div>
+        <div className="expense-item__price">{`${amount} ₽`}</div>
       </div>
     </div>
   );
