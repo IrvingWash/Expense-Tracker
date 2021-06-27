4c443a48af0ac1e57d6a3c0751b541ea660eda97
Washington Irving <iamwashingyou@gmail.com>
2021-06-28 00:34:46 +0300

    added a button to hide the form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,12 @@
 import './NewExpense.css';
 
+import { useState } from 'react';
+
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+  const [formIsShown, setFormIsShown] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -10,13 +14,28 @@ const NewExpense = (props) => {
     };
 
     props.onAddExpense(expenseData);
+
+    setFormIsShown(false);
   };
 
-  return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
+  const showFormHandler = () => {
+    setFormIsShown(true);
+  };
+
+  const hideFormHandler = () => {
+    setFormIsShown(false);
+  };
+
+  const content = formIsShown ? (
+    <ExpenseForm
+      onSaveExpenseData={saveExpenseDataHandler}
+      onHideForm={hideFormHandler}
+    />
+  ) : (
+    <button onClick={showFormHandler}>Add New Expense</button>
   );
+
+  return <div className="new-expense">{content}</div>;
 };
 
 export default NewExpense;
